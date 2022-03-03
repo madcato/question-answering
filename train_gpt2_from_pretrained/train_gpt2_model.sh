ef1c0ccfa2eee211c6657b425c8cff75cfcb9f20
Daniel Vela <veladan@me.com>
2022-03-03 12:13:32 +0100

    tiny-gpt2

--- train_gpt2_from_pretrained/train_gpt2_model.sh
+++ train_gpt2_from_pretrained/train_gpt2_model.sh
@@ -3,7 +3,7 @@ export VAL_FILE=val_list.txt
 
 # Train GPT2 model from scratch with custom train files
 CUDA_LAUNCH_BLOCKING=1 python3 ./transformers/examples/pytorch/language-modeling/run_clm.py \
-    --model_name_or_path gpt2 \
+    --model_name_or_path sshleifer/tiny-gpt2 \
     --train_file $TRAIN_FILE \
     --validation_file=$VAL_FILE \
     --per_device_train_batch_size 64 \
