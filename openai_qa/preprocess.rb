5371b59b2cb4338700a14d1c432913d46121a8a9
Daniel Vela <veladan@me.com>
2023-01-13 11:28:19 +0100

    solve embeddings generation with sentece-flask

--- openai_qa/preprocess.rb
+++ openai_qa/preprocess.rb
@@ -5,13 +5,15 @@ Dotenv.load
 require 'daru'
 require "numo/narray"
 require './openai_utils'
+require 'httparty'
 
+MODE="FLASK" # "OPENAI" or "FLASK"
 ### Load csv with Daru
 data = Daru::DataFrame.from_csv('https://cdn.openai.com/API/examples/data/olympics_sections_text.csv')
 
 ## Daru set index
 data = data.set_index(["title", "heading"])
-data = data.first(500)
+# data = data.first(500)
 
 puts "#{data.size} rows in the data."
 
@@ -20,13 +22,23 @@ puts "#{data.size} rows in the data."
 puts
 puts
 
-# Embeddings
+if MODE == "OPENAI"
+  # Embeddings
+  openai = OpenAI_API.new(DOC_EMBEDDINGS_MODEL, QUERY_EMBEDDINGS_MODEL)
+  ## Generate embeddings for each row of the daru dataframe
+  context_embeddings = openai.compute_doc_embedding(data)
+elsif MODE == "FLASK"
+  context_embeddings = data.map_rows do |row|
+    row_text = row['content'].gsub("\n", " ")
+    # Make a post to http://localhost:3001/ with json contenttype to send row_text
+    # and get the embedding
+    response = HTTParty.post('http://localhost:3001/', body: {sentences: row_text}.to_json, headers: { 'Content-Type' => 'application/json' })
+    response.parsed_response 
+  end
 
-openai = OpenAI_API.new(DOC_EMBEDDINGS_MODEL, QUERY_EMBEDDINGS_MODEL)
+end
 
-## Generate embeddings for each row of the daru dataframe
-context_embeddings = openai.compute_doc_embedding(data)
-
+p context_embeddings
 p "context_embeddings.size #{context_embeddings.size}"
 
 ## Add embeddings to the dataframe
@@ -40,7 +52,12 @@ def vector_similarity(x, y)
 end
 
 def order_document_sections_by_query_similarity(openai, query, context)
-  query_embedding = openai.generate_query_embedding(query)
+  if MODE == "OPENAI"
+    query_embedding = openai.generate_query_embedding(query)
+  elsif MODE == "FLASK"
+    response = HTTParty.post('http://localhost:3001/', body: {sentences: query}.to_json, headers: { 'Content-Type' => 'application/json' })
+    query_embedding = response.parsed_response
+  end
   contextmapsim = context.map_rows do |row|
     vector_similarity(query_embedding, row[:embedding])
   end
